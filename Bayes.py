322bc1d54ae0939175bf9d164d1ac700c9fc5c58
nickperga <nikolaospergantes@gmail.com>
2023-12-14 01:08:57 +0200

    Just created the 0,1 files for our vocabulary containing in the train examples

--- Bayes.py
+++ Bayes.py
@@ -9,15 +9,12 @@ with open("most_common_words.csv", 'r') as file:
   for row in csvreader:
     l.append(row[0])
 
-
-print (l)
-DIR = "aclImdb\\train\\pos\\"
+DIR = "aclImdb\\train\\neg\\"
 good = []
-list1 = os.listdir(DIR)[:10]
+list1 = os.listdir(DIR)
 for filename in list1:
-    words = re.findall(r'\w+', open("aclImdb\\train\\pos\\" + filename, encoding="utf8").read().lower())
+    words = re.findall(r'\w+', open("aclImdb\\train\\neg\\" + filename, encoding="utf8").read().lower())
     vec = []
-    print(words)
     for i in l:
         if i in words:
           vec.append(1)
@@ -25,7 +22,7 @@ for filename in list1:
           vec.append(0)
     good.append(vec)
 
-with open('positive.csv', 'w', newline='') as csv_file:  
+with open('negative.csv', 'w', newline='') as csv_file:  
     writer = csv.writer(csv_file)
     writer.writerows(good)
 
