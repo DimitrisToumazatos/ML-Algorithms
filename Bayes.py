e6edb39b0458f82dce0ee0e1dd44ca15ba01e21f
nickperga <nikolaospergantes@gmail.com>
2023-12-17 13:08:35 +0200

    Finished the makeData file and here is its product

--- Bayes.py
+++ Bayes.py
@@ -1,36 +1,115 @@
 import csv
-import os
-import re
-import collections
-
-l = []
-with open("most_common_words.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  for row in csvreader:
-    l.append(row[0])
-
-DIR = "aclImdb\\train\\neg\\"
-good = []
-list1 = os.listdir(DIR)
-for filename in list1:
-    words = re.findall(r'\w+', open("aclImdb\\train\\neg\\" + filename, encoding="utf8").read().lower())
-    vec = []
-    for i in l:
-        if i in words:
-          vec.append(1)
-        else:
-          vec.append(0)
-    good.append(vec)
-
-with open('negative.csv', 'w', newline='') as csv_file:  
-    writer = csv.writer(csv_file)
-    writer.writerows(good)
+from operator import add
+import pandas as pd
+from math import log2
 
 
-"""
+
 class NaiveBayes:
-   
-   def __init__(self) -> None:
-      pass
-   """
+  pos_vocabulary_prob = []
+  neg_vocabulary_prob = []
+  prob_pos = 0
+  prob_neg = 0
+
+
+  def __init__(self):
+    pass
+
+  def train(self, pos_filename, neg_filename):
+    flag = 0
+    nPos = 0
+    nNeg = 0
+    pos_vocabulary_count = []
+    neg_vocabulary_count = []
+    with open(pos_filename, 'r') as file:
+      csvreader = csv.reader(file)
+      for row in csvreader:
+        row = [int(x) for x in row]
+        nPos += 1
+        if (flag == 0):
+          flag = 1
+          pos_vocabulary_count = [0 for i in range(len(row))]
+          neg_vocabulary_count = [0 for i in range(len(row))]
+        pos_vocabulary_count = list(map( add, pos_vocabulary_count, row))
+    
+
+
+    with open(neg_filename, 'r') as file:
+      csvreader = csv.reader(file)
+      
+      for row in csvreader:
+        row = [int(x) for x in row]
+        nNeg += 1
+        neg_vocabulary_count = list(map( add, neg_vocabulary_count, row))
+
+    self.prob_pos = nPos / (nNeg + nPos)
+    self.prob_neg = nNeg / (nNeg + nPos)
+
+    self.pos_vocabulary_prob = [x / nPos for x in pos_vocabulary_count]
+    self.neg_vocabulary_prob = [x / nNeg for x in neg_vocabulary_count]
+
+    with open('modelData.csv', 'w', newline='', encoding="utf8") as csv_file:        # save model
+      writer = csv.writer(csv_file)                
+      writer.writerow(self.pos_vocabulary_prob)
+      writer.writerow(self.neg_vocabulary_prob)
+      writer.writerow([self.prob_pos])
+      writer.writerow([self.prob_neg])
+
+  def test(self, pos_filename, neg_filename):
+    nPos = 0
+    nNeg = 0
+    countPos = 0
+    countNeg = 0
+    with open(pos_filename, 'r') as file:
+      csvreader = csv.reader(file)
+      for row in csvreader:
+        nPos += 1
+        row = [int(x) for x in row]
+        probP = self.prob_pos * self.calculateProb(1, row)
+        probN = self.prob_neg * self.calculateProb(0, row)
+        
+        if probP >= probN:
+          countPos += 1
+
+    countNeg = 0
+    with open(neg_filename, 'r') as file:
+      csvreader = csv.reader(file)
+      for row in csvreader:
+        nNeg += 1
+        row = [int(x) for x in row]
+        probP = self.prob_pos * self.calculateProb(1, row)
+        probN = self.prob_neg * self.calculateProb(0, row)
+        
+        if probP <= probN:
+          countNeg += 1
+      
+
+    print("For the positive examples the accuracy is: " + str(probP/nPos))  
+    print("For the negetive examples the accuracy is: " + str(probN/nNeg))  
+
+  def calculateProb(self, category, row):
+    prob = 0
+    if category == 1:
+      l = self.pos_vocabulary_prob
+    else:
+      l = self.neg_vocabulary_prob
+
+    index = 0
+    for i in row:
+
+      if i == 1:
+        prob += log2(l[index] + 1)
+      else:
+        prob += log2((1 - l[index]) + 1)
+
+      index += 1
+
+    return prob
+
+
+Nb = NaiveBayes()
+Nb.train("positive.csv", "negative.csv")
+Nb.test("positiveDev.csv", "negativeDev.csv")
+
+
 
---
+++ makeTrainData.py
@@ -0,0 +1,64 @@
+import re
+import os
+from operator import itemgetter
+import csv
+
+n = 29
+m = 1500
+k = 0
+
+
+posNegList = ["pos", "neg"]
+dict1 = {}
+s1 = "aclImdb\\train\\"
+s2= "\\"
+for i in posNegList:                    # read training data and calculate count per word 
+    DIR = s1+i+s2
+    list1 = os.listdir(DIR)
+    for filename in list1:
+        words = re.findall(r'\w+', open(s1+i+s2 + filename, encoding="utf8").read().lower())
+        for word in words:
+            if len(word)>2:
+                if word in dict1:
+                    dict1[word] += 1
+                else:
+                    dict1[word] = 1
+
+most_common = dict(sorted(dict1.items(), key=itemgetter(1), reverse=True)[:m]) # create and sort dictionary of most common words and their count 
+
+
+l = list(most_common.keys())[n:]
+
+
+
+with open('most_common_words.csv', 'w', newline='', encoding="utf8") as csv_file:        # write the most common words in a file
+    writer = csv.writer(csv_file)
+    for i in l:                   
+        writer.writerow([i])
+
+
+for i in posNegList:            # create verctors for the traing data (positie/negative) 
+
+    DIR = s1+i+s2
+    train = []
+    list1 = os.listdir(DIR)     #a function which creates a list which contains the file names in the DIR folder 
+    for filename in list1:
+        words = re.findall(r'\w+', open(s1+i+s2 + filename, encoding="utf8").read().lower())
+        vec = []
+        for j in l:
+            if j in words:
+                vec.append(1)
+            else:
+                vec.append(0)
+        train.append(vec)
+
+    if i == "pos":
+        temp = "positive.csv"
+    else:
+        temp = "negative.csv"
+        
+    with open(temp, 'w', newline='') as csv_file:  
+        writer = csv.writer(csv_file)
+        writer.writerows(train)
+
+
