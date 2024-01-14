4299fbdf6c1f7222bb0c8d3c520f0c4eac1d6ad5
nickperga <nikolaospergantes@gmail.com>
2024-01-14 21:29:22 +0200

    Changed the Execution file so we can run all the algorithms together

--- ExecutionFile.py
+++ ExecutionFile.py
@@ -1,121 +1,147 @@
 from Bayes import *
 from AdaBoost import *
+from randomForest import *
 import pandas as pd
 import copy
 
-PosTrainData = []
-NegTrainData = []
+posTrainData = []
+negTrainData = []
 #Reading of the Positive Training Data
 print("Reading of the positive training data!")
 
 dfPos = pd.read_csv("positiveTrain.csv", header = None)     # read the positive training examples
-sizePos = dfPos.shape[0]        # shape[0] contains the number of rows of our file
+sizePosTrain = dfPos.shape[0]        # shape[0] contains the number of rows of our file
+colPos = dfPos.shape[1]         # shape[1] contains the number of columns of our file
 
-for i in range(sizePos):
-    row = [int(x) for x in dfPos.iloc[i, : ]]  # df.iloc function returns the row of our csv that we tell it to give. 
-    PosTrainData.append(row)
+for i in range(sizePosTrain):
+    row = [int(x) for x in dfPos.iloc[i, : ]]  # df.iloc function returns the row of our csv that i shows 
+    posTrainData.append(row)
 
 #Reading of the Negative Training Data
 print("Reading of the negative training data!")
 
 dfNeg = pd.read_csv("negativeTrain.csv", header = None)     # read the negative training examples
-sizeNeg = dfNeg.shape[0]        # shape[0] contains the number of rows of our file
+sizeNegTrain = dfNeg.shape[0]        # shape[0] contains the number of rows of our file
+colNeg = dfNeg.shape[1]         # shape[1] contains the number of columns of our file
 
-for i in range(sizeNeg):
-    row = [int(x) for x in dfNeg.iloc[i, : ]]
-    NegTrainData.append(row)
+for i in range(sizeNegTrain):
+    row = [int(x) for x in dfNeg.iloc[i, : ]]       # df.iloc function returns the row of our csv that i shows
+    negTrainData.append(row)
 
-Train_Data = copy.deepcopy(PosTrainData)
-for row in NegTrainData:
+#Bayes initialization and Training
+nB = NaiveBayes()
+nB.train(posTrainData, colPos, negTrainData, colNeg)
+
+#Train Data modifications
+Train_Data = copy.deepcopy(posTrainData)    #creates a deepcopy of the posTrainData
+for row in negTrainData:
     Train_Data.append(row)
 results = []
-for i in range(len(PosTrainData)):
+for i in range(len(posTrainData)):
     results.append(1)
-for i in range(len(NegTrainData)):
+for i in range(len(negTrainData)):
     results.append(0)
 
+#Random forest initialization and Training
+rf = RandomForest(101, 4)
+rf.train(Train_Data, results, colNeg)
+
+#AdaBoost initialization and training
 Ada = AdaBoost(100)
 Ada.fit(Train_Data, results)
 
-PosTestData = []
-NegTestData = []
+
+
+posTestData = []
+negTestData = []
 #Reading of the Positive Test Data
-print("Reading the positive test data!")
+print("Reading of the positive test data!")
 
 dfPos = pd.read_csv("positiveTest.csv", header = None)     # read the positive test examples
-sizePos = dfPos.shape[0]                                  # shape[0] contains the number of rows of our file
+sizePosTest = dfPos.shape[0]                                  # shape[0] contains the number of rows of our file
 
-for i in range(sizePos):
+for i in range(sizePosTest):
     row = [int(x) for x in dfPos.iloc[i, : ]]  # df.iloc function returns the row of our csv that we tell it to give. 
-    PosTestData.append(row)
+    posTestData.append(row)
 
 #Reading of the Negative Test Data
 print("Reading of the negative test data!")
 
 dfNeg = pd.read_csv("negativeTest.csv", header = None)     # read the negative test examples
-sizeNeg = dfNeg.shape[0]                                  # shape[0] contains the number of rows of our file
+sizeNegTest = dfNeg.shape[0]                                  # shape[0] contains the number of rows of our file
 
-for i in range(sizeNeg):
+for i in range(sizeNegTest):
     row = [int(x) for x in dfNeg.iloc[i, : ]]
-    NegTestData.append(row)
+    negTestData.append(row)
 
-
-"""
-myNaiveBayes = NaiveBayes()
-myNaiveBayes.train(PosTrainData, dfPos.shape[1], NegTrainData, dfNeg.shape[1])
-
-with open('myBayes-130-1700.pkl', 'wb') as outp:
-    pickle.dump(myNaiveBayes, outp, pickle.HIGHEST_PROTOCOL)
-"""
-"""testData = copy.deepcopy(PosTestData)
-for row in NegTestData:
+#Test Data modifications
+testData = copy.deepcopy(posTestData)
+for row in negTestData:
     testData.append(row)
 
 results = []
-numberOfPositive = len(PosTestData)
-numberOfNegative = len(NegTestData)
-for i in range(numberOfPositive):
+sizePosTest = len(posTestData)
+sizeNegTest = len(negTestData)
+for i in range(sizePosTest):
     results.append(1)
-for i in range(numberOfNegative):
+for i in range(sizeNegTest):
     results.append(0)
-"""
 
-res = Ada.predict(PosTestData)
+#Naive Bayes Test
+nBpositive, nBnegative = nB.test(posTestData, negTestData)
+falsePositive = sizeNegTest - nBnegative
+falseNegative = sizePosTest - nBpositive
+
+# print test statistics
+print("Naive bayes statistics for " + str(sizeNegTrain + sizePosTrain) + " train examples are the above:")
+print("True Positive: " + str(nBpositive))
+print("False Positive: " + str(falsePositive))
+print("True Negative: " + str(nBnegative))
+print("False Negative: " + str(falseNegative))
+print("The accuracy for the positive data is: " + str(round(((nBpositive / sizePosTest) * 100), 2)))   # 84.58 
+print("The accuracy for the negative data is: " + str(round(((nBnegative / sizeNegTest) * 100), 2)))  # 78.3
+print("The total accuracy is: " + str(round((((nBnegative + nBpositive) / (sizeNegTest + sizePosTest)) * 100), 2)))  
+precision = round((nBpositive / (nBpositive + falsePositive)), 3)
+print("For the positive data the precision is: " + str(precision))  
+recall =  round((nBpositive / (nBpositive + falseNegative)), 3)
+print("For the positive data the recall is: " + str(recall))    
+print("The F1 for the negative test data is: " + str(round(2 / (1 / recall + 1 / precision), 3)))
+
+#Random forest test
+rfpositive, rfnegative = rf.test(testData)
+falsePositive = sizeNegTest -rfnegative
+falseNegative = sizePosTest - rfpositive
+
+# print train statistics
+print("Random Forest statistics for " + str(sizeNegTrain + sizePosTrain) + " train examples are the above:")
+print("True Positive: " + str(rfpositive))
+print("False Positive: " + str(falsePositive))
+print("True Negative: " + str(rfnegative))
+print("False Negative: " + str(falseNegative))
+print("The accuracy for the positive data is: " + str(round(((rfpositive / sizePosTest)), 3)))
+print("The accuracy for the negative data is: " + str(round(((rfnegative / sizePosTest)), 3)))
+print("The total accuracy is: " + str(round((((rfnegative + rfpositive) / (sizeNegTest + sizePosTest))), 3)))  
+precision = round((rfpositive / (rfpositive + falsePositive)), 3)
+print("For the positive data the precision is: " + str(precision))  
+recall =  round((rfpositive / (rfpositive + falseNegative)), 3)
+print("For the positive data the recall is: " + str(recall))    
+print("The F1 for the negative test data is: " + str(round(2 / (1 / recall + 1 / precision), 3)))
+
+
+res = Ada.predict(posTestData)
 correct = 0
 for i in res:
     if (i == 1):
         correct += 1
 
-print("The accuracy for the positive test data was " + str(correct / len(PosTestData)))
+print("The accuracy for the positive test data was " + str(correct / len(posTestData)))
 
-res = Ada.predict(NegTestData)
+res = Ada.predict(negTestData)
 correct = 0
 for i in res:
     if (i == 0):
         correct += 1
 
 
-print("The accuracy for the negative test data was " + str(correct / len(NegTestData)))
-"""
-print("Save model...")
+print("The accuracy for the negative test data was " + str(correct / len(negTestData)))
 
-with open('myAdaBoost2.pkl', 'wb') as outp:            # save object 
-    pickle.dump(Ada, outp, pickle.HIGHEST_PROTOCOL) 
-    
-with open('myAdaBoost.pkl', 'rb') as inp:             # read saved object
-    Ada = pickle.load(inp)
-"""
-
-
-
-
-"""
-#Calling the Bayes algorithm 
-print("Now we run the Bayes algorithm")
-
-with open('myBayes-130-1700.pkl', 'rb') as inp:
-    myNaiveBayes = pickle.load(inp)
-
-
-myNaiveBayes.test(posTestData, negTestData)
-"""
