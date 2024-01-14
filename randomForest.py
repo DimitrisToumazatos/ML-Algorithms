8d7ca34ed99490558c5b92c1ab2aad3376595a8b
Dimitris Toumazatos <di.toumazatos@gmail.com>
2024-01-14 19:24:44 +0200

    fix randomForest

--- randomForest.py
+++ randomForest.py
@@ -3,63 +3,58 @@ from random import randint
 import numpy as np
 from operator import add
 
-k=8000             # Number of examples in each training category
-
 class RandomForest:
 
+    # Constructor.
+    # Sets up the Random Forest Object
     def __init__(self, n, m):
-        #The init of the rf sets the
-        #number of trees created and the
-        #number of features used
-        self.nTrees = n                 # number of trees
-        self.mFeatures = m              # number of features        
-        self.trees = []                 # list containing the trees
-
-    def train(self, x_train, y_train, columns):
-        #We create the trees with the m random features
-        #using the id3 algorithm
+        self.nTrees = n                 # set the number of trees
+        self.mFeatures = m              # set the number of features        
+        self.trees = []                 # set the list containing the trees
+    
+    # Training Function.
+    # Create n trees with m random features
+    # using the id3 algorithm
+    def train(self, x_train, y_train):
         print("Training started")
         print("Creating Trees...")
 
-        self.trainingList = np.array(x_train)   # list containing the vectors for the training examples
-        self.trainingListResults = np.array(y_train)    # list containing the correct category for each of the above examples
+        x_train = np.array(x_train)           # list containing the vectors for the training examples.
+        y_train = np.array(y_train)           # list containing the correct category for each of the above examples.
 
-        for i in range(self.nTrees):
+        numberOfFeatures = len(x_train[0])
 
-            features = [randint(0, int((columns)-1)) for j in range(self.mFeatures)]        # selects the m random features
-            self.trees.append(ID3(np.array(features)))      #creates the new tree
-            self.trees[i].fit(self.trainingList, self.trainingListResults) 
+        for i in range(self.nTrees):            # Create the trees
+
+            features = [randint(0, int((numberOfFeatures)-1)) for j in range(self.mFeatures)]         # Select m random features
+            self.trees.append(ID3(np.array(features)))                                                # and create a new tree.
+            self.trees[i].fit(x_train, y_train)                                                       # Train the new tree.
             print("Tree number " + str(i) + " has been created!")
 
         print("Training finished")
 
-                                                    
+    # Test the User's input.                                
     def test(self, testData):
 
         print("Test started")
 
         print("Calculating output...")
 
-        treeOut = [0] * len(testData)       # predict the estimated results for each examples
-        testData = np.array(testData)       #convering the testData list to np array
+        treeOut = [0] * len(testData)                   # Create a list for the trees's predictions.
+        testData = np.array(testData)                   # Convey the testData list to np array.
         
-        for t in self.trees:   
-            temp = list(t.predict(testData))    #returns a list with the predicted results of a tree
-            treeOut = list(map(add, treeOut, temp))     #adds the results of the trees for the testData
+        for tree in self.trees:   
+            temp = list(tree.predict(testData))         # Create a list with the predicted results of a tree
+                                                        # for every given example.
+            treeOut = list(map(add, treeOut, temp))     # adds the results of the trees.
 
-        rfPositive = 0
-        rfNegative = 0
-
-        count = 0
-        for i in treeOut:                          # gets the accuracy of the model
-            if count < round(len(testData) / 2):    #Τουμ βαλε εδώ σχόλια δε θυμάμαι καλά
-                if i > self.nTrees/2:
-                    rfPositive += 1
-            else:
-                if i < self.nTrees:
-                    rfNegative += 1
-            count+=1
+        prediction = []
+        for i in treeOut:                               # Make predictions for each example.
+            if sum(i) > self.nTrees/2:                  # The example is deemed as positive.
+                prediction.append(1)
+            else:                                       # The example is deemed as negative.
+                prediction.append(0)
 
         print("Test finished")
 
-        return rfPositive, rfNegative
+        return prediction
